9b0b8128ca9987557f14e111e3a278a6f4ab3f9a
Carl J <carlj1881@yahoo.com>
2020-09-24 19:46:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,13 +4,11 @@ class Dog
   
   def initialize(name, breed)
     @name = name
-    if breed != nil
-           @breed = breed
-         else
+    if breed == nil
       breed = "Mutt"
+    else
 @breed = breed
   end
-end
 
 
   def name=(name)
